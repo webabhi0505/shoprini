1f855b76df5f04566cc0fa7ad6b7aa9f01eb78e7
webabhi0505 <135849277+webabhi0505@users.noreply.github.com>
2023-07-10 20:58:44 +0530

    Add files via upload

---
+++ content.js
@@ -0,0 +1,91 @@
+// console.clear();
+
+let contentTitle;
+
+console.log(document.cookie);
+function dynamicclthsSection(ob) {
+  let boxDiv = document.createElement("div");
+  boxDiv.id = "box";
+
+  let boxLink = document.createElement("a");
+  // boxLink.href = '#'
+  boxLink.href = "/about.html?" + ob.id;
+  // console.log('link=>' + boxLink);
+
+  let imgTag = document.createElement("img");
+  // imgTag.id = 'image1'
+  // imgTag.id = ob.photos
+  imgTag.src = ob.preview;
+
+  let detailsDiv = document.createElement("div");
+  detailsDiv.id = "details";
+
+  let h3 = document.createElement("h3");
+  let h3Text = document.createTextNode(ob.name);
+  h3.appendChild(h3Text);
+
+  let h4 = document.createElement("h4");
+  let h4Text = document.createTextNode(ob.brand);
+  h4.appendChild(h4Text);
+
+  let h2 = document.createElement("h2");
+  let h2Text = document.createTextNode("rs  " + ob.price);
+  h2.appendChild(h2Text);
+
+  boxDiv.appendChild(boxLink);
+  boxLink.appendChild(imgTag);
+  boxLink.appendChild(detailsDiv);
+  detailsDiv.appendChild(h3);
+  detailsDiv.appendChild(h4);
+  detailsDiv.appendChild(h2);
+
+  return boxDiv;
+}
+
+//  TO SHOW THE RENDERED CODE IN CONSOLE
+// console.log(dynamicclthsSection());
+
+// console.log(boxDiv)
+
+let main3 = document.getElementById("main3");
+let containerclths = document.getElementById("containerclths");
+let containerAccessories = document.getElementById("containerAccessories");
+// main3.appendChild(dynamicclthsSection('hello world!!'))
+
+// BACKEND CALLING
+
+let httpRequest = new XMLHttpRequest();
+
+httpRequest.onreadystatechange = function() {
+  if (this.readyState === 4) {
+    if (this.status == 200) {
+      // console.log('call successful');
+      contentTitle = JSON.parse(this.responseText);
+      if (document.cookie.indexOf(",counter=") >= 0) {
+        var counter = document.cookie.split(",")[1].split("=")[1];
+        document.getElementById("badge").innerHTML = counter;
+      }
+      for (let i = 0; i < contentTitle.length; i++) {
+        if (contentTitle[i].isAccessory) {
+          console.log(contentTitle[i]);
+          containerAccessories.appendChild(
+            dynamicclthsSection(contentTitle[i])
+          );
+        } else {
+          console.log(contentTitle[i]);
+          containerclths.appendChild(
+            dynamicclthsSection(contentTitle[i])
+          );
+        }
+      }
+    } else {
+      console.log("call failed!");
+    }
+  }
+};
+httpRequest.open(
+  "GET",
+  "https://5d76bf96515d1a0014085cf9.mockapi.io/product",
+  true
+);
+httpRequest.send();
